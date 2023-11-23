63914c8c965ea15d08fc49a463a75866915466cb
kamikazi_Deborah <deborahkamikazi9@gmail.com>
2023-11-23 06:33:04 -0800

    variables,boolean,comparision

---
+++ DAY3/Day3.js
@@ -0,0 +1,26 @@
+//declarind variables as constant//
+const num = 2;
+console.log(num);
+//declare avaliable and assign number//
+var num1 = 5;
+console.log("this the number"+ num);
+
+let num2 = 5;
+num2 = 3;
+console.log(num2);
+//boolean //
+let isonline = true;
+console.log(isonline);
+isonline = false;
+console.log(isonline);
+//boolean function falsy and truely//
+console.log(Boolean(100));
+console.log(Boolean(-1));
+console.log(Boolean(1));
+console.log(Boolean(0));
+let names = "james";
+console.log(Boolean(names));
+//comperision operator//
+const priceOne = 5;
+const priceTwo = 5;
+console.log(priceOne==priceTwo);
