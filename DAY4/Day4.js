fa38d6b41e3b21eefa7547ed341ce549b1337e43
kamikazi_Deborah <deborahkamikazi9@gmail.com>
2023-11-27 06:27:13 -0800

    functions

---
+++ DAY4/Day4.js
@@ -0,0 +1,36 @@
+//operators//
+const numOne = 5;
+const numTwo = 6;
+console.log(numOne==numTwo);
+console.log(numOne<numTwo);
+console.log(numTwo>numOne);
+console.log(numTwo<=numOne);
+console.log(numTwo>=numOne);
+//use of typeof//
+ const num = 3;
+ console.log(num);
+
+
+let text ="kami";
+console.log(text);
+
+ let height = 180;
+ let anotherheight = height;
+ let weight;
+ console.log(height);
+ console.log(anotherheight);
+ weight = 40;
+ console.log(weight);
+ //function//
+ var globalgreeting = "Good";
+ function  testfunction(){
+    var localgreeting = "morning";
+    console.log("function");
+    console.log(globalgreeting);
+    console.log(localgreeting);
+ }
+ testfunction();
+ console.log("main program");
+ console.log(globalgreeting);
+ 
+ 
